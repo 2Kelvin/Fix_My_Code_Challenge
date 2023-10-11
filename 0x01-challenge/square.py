2fcb64e7a8b1b62731eb43e2f26966fd8b4cea72
2Kelvin <rocketmancodes02@gmail.com>
2023-10-11 04:26:31 -0700

    add docstrings

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
+"""Class Square module"""
 
 class square():
-    
+    """class Square"""
     width = 0
     height = 0
 
@@ -17,13 +18,15 @@ class square():
         return self.width * self.width
 
     def PermiterOfMySquare(self):
+        """Calculates the perimeter of a square"""
         return (self.width * 2) + (self.height * 2)
 
     def __str__(self):
+        """Custom string object"""
         return "{}/{}".format(self.width, self.height)
 
 if __name__ == "__main__":
-
+    """Code should not run when imported"""
     s = square(width=12, height=9)
     print(s)
     print(s.area_of_my_square())
