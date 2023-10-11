8c9d7d6c310783fcb51014dd390b85ba252c7fe8
2Kelvin <rocketmancodes02@gmail.com>
2023-10-11 08:14:56 -0700

    edit string docs

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -3,34 +3,33 @@
 
 
 class Square():
-    """ Class Square to create squares """
+    """Class Square to create squares."""
     width = 0
     height = 0
 
     def __init__(self, *args, **kwargs):
-        """ Init Square method to create square instances """
+        """Init Square method to create square instances."""
         for key, value in kwargs.items():
             setattr(self, key, value)
         if not hasattr(self, "height"):
             self.height = self.width
 
     def area_of_my_square(self):
-        """ Area of the square """
+        """Area of the square."""
         return self.width * self.width
 
-    def permiterOfMySquare(self):
-        """ Calculates the perimeter of a square """
+    def perimeterOfMySquare(self):
+        """Calculates the perimeter of a square."""
         return (self.width * 4)
 
     def __str__(self):
-        """ Custom string object """
+        """Custom string object."""
         return "{}/{}".format(self.width, self.height)
 
 
 if __name__ == "__main__":
-    """ Code should not run when imported """
-    s = Square(width=12, height=9)
+    """Code should not run when imported."""
+    s = Square(width=12, height=12)
     print(s)
     print(s.area_of_my_square())
-    print(s.permiterOfMySquare())
-
+    print(s.perimeterOfMySquare())
